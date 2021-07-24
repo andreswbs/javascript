5390f68d4b13cb69c5c011e3364d76b6edfe4301
Andres Järviste WBS <andres.jarviste@wbscodingschool.com>
2021-07-24 16:55:41 +0300

    checkbox questions handling

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -5,25 +5,38 @@ class Question {
     this._id = id;
   }
 
+  get id() {
+    return this._id;
+  }
+
   set question(param) {
     this._question = param;
   }
 
   set correctAnswer(param) {
-    this._correctAnswer = param;
+    this._correctAnswer = param.toLowerCase();
   }
 
   makeQuestionOputput() {
-    return `<div>${this._question}</div>`;
+    return `<div class="test-question">${this._question}</div>`;
   }
 
   makeAnswerOutput() {
     return `
-            <div>
+            <div class="test-answer-output">
                 <span>Your answer:</span>
                 <input type="text" id="answer${this._id}">
             </div>
         `;
+  }
+
+  getUserInput() {
+    return document.getElementById("answer" + this._id).valuetoLowerCase();
+  }
+
+  compareAnswer() {
+    console.log("USer input: " + this.getUserInput());
+    return this._correctAnswer == this.getUserInput();
   }
 
   makeCorrectAnswerOutput() {
@@ -38,25 +51,52 @@ class RadioQuestion extends Question {
   }
 
   _makeOneRadioOutput(value, radioId) {
-      const name = "radio-" + radioId
-      return `
+    const name = `multi-${this._id}-radio-${radioId}`;
+    return `
         <input type="radio" id="${name}" name="multi-${this._id}" value="${value}">
         <label for="${name}">${value}</label><br>
-      `
+      `;
   }
 
   addChoice(choice) {
-      this.choices.push(choice)
+    this.choices.push(choice);
+  }
+
+  getUserInput() {
+    return document.querySelector(`input[name="multi-${this._id}"]:checked`)
+      .value.toLowerCase();
   }
 
   makeAnswerOutput() {
-    let output = "";
+    let output = '<div class="test-answer-output">';
     this.choices.forEach((radio, index) => {
-      output += this._makeOneRadioOutput(radio, index)
+      output += this._makeOneRadioOutput(radio, index);
     });
+    output += "</div>";
     return output;
   }
+}
 
+class CheckBoxQuestion extends RadioQuestion {
+  set correctAnswer(param) {
+    this._correctAnswer = param.sort().toString();
+  }
+
+  _makeOneRadioOutput(value, radioId) {
+    const name = `multi-${this._id}-radio-${radioId}`;
+    return `
+      <input type="checkbox" id="${name}" name="multi-${this._id}" value="${value}">
+      <label for="${name}">${value}</label><br>
+    `;
+  }
+
+  getUserInput() {
+    const checkedNodeList = document.querySelectorAll(`input[name="multi-${this._id}"]:checked`)
+    const checkedElemsArray = Array.from(checkedNodeList)
+    const checkedValues = checkedElemsArray.map( elem => elem.value )
+    return checkedValues.sort().toString()
+  }
+  
 }
 
 class Test {
@@ -75,6 +115,60 @@ class Test {
     });
     return output;
   }
+
+  _makeNavButtons(index) {
+    let output = "";
+    const questionId = this.questionsArray[index].id;
+
+    output += `
+      <div class="test-validate">
+        <a href="#" onclick="validateAnswer(${questionId})">Check</a>
+      </div>
+    `;
+    if (index > 0) {
+      const prevQuestionId = this.questionsArray[index - 1].id;
+      output += `
+        <a href="#" onclick="renderTest(${prevQuestionId})" >Previous</a>
+      `;
+    }
+
+    if (index < this.questionsArray.length - 1) {
+      const nextQuestionId = this.questionsArray[index + 1].id;
+      output += `
+        <a href="#" onclick="renderTest(${nextQuestionId})" >Next</a>
+      `;
+    }
+
+    return output;
+  }
+
+  makeOuptuForOneTest(testUI) {
+    let output = "";
+    let index = this.questionsArray.findIndex(function (question) {
+      return testUI === question.id;
+    });
+
+    if (index < 0) {
+      return "No test with id" + testUI;
+    }
+
+    const question = this.questionsArray[index];
+    output += question.makeQuestionOputput();
+    output += question.makeAnswerOutput();
+    output += `<div id="answerReveal"></div>`;
+    output += this._makeNavButtons(index);
+
+    return output;
+  }
+
+  validateAnswer(id) {
+    let foundQ = this.questionsArray.find(function (question) {
+      return id === question.id;
+    });
+
+    console.log(foundQ.compareAnswer());
+    return foundQ.compareAnswer();
+  }
 }
 
 const q1 = new Question(1);
@@ -87,13 +181,42 @@ q2.correctAnswer = "a ring";
 
 const q3 = new RadioQuestion(3);
 q3.question = "What is a capital of Germany?";
-q3.choices = ["Berlin", "Vienna", "Hamburg"]
+q3.choices = ["Berlin", "Vienna", "Hamburg"];
 q3.correctAnswer = "Berlin";
+
+const q4 = new RadioQuestion(4);
+q4.question = "What is a capital of England?";
+q4.choices = ["Berlin", "London", "Bath"];
+q4.correctAnswer = "London";
+
+const q5 = new CheckBoxQuestion(5);
+q5.question = "Which are vegetables?";
+q5.choices = ["Orange", "Potato", "Carrot", "Cat", "Bicycle"];
+q5.correctAnswer = ["Carrot", "Potato"];
 
 const test = new Test();
 test.addQuestion(q1);
 test.addQuestion(q2);
 test.addQuestion(q3);
+test.addQuestion(q4);
+test.addQuestion(q5);
 
-const testElem = document.getElementById("test");
-testElem.innerHTML = test.makeTestOutput();
+function renderTest(id) {
+  const testElem = document.getElementById("test");
+  testElem.innerHTML = test.makeOuptuForOneTest(id);
+  console.log("render test for" + id);
+}
+
+function validateAnswer(id) {
+  let isCorrect = test.validateAnswer(id);
+  let output = "";
+
+  if (isCorrect) {
+    output = "Correct!";
+  } else {
+    output = "Wrong!";
+  }
+  document.getElementById("answerReveal").innerText = output;
+}
+
+renderTest(2);
